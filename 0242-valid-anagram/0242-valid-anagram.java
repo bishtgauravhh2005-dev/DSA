954c1638d496182a4ac089147a11e503ce7c56fd
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-06-23 21:59:03 +0530

    Time: 439 ms (5.44%), Space: 46.5 MB (24.39%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        List<Character> l = new ArrayList<>();
+        int n = s.length();
+        int m = t.length();
+        if(n != m ){
+            return false;
+        }
+        for(int i=0 ;i<n;i++){
+                l.add(s.charAt(i));
+            
+        }
+        for(int i =0 ; i<m ;i++){
+            if(!l.contains(t.charAt(i))){
+                return false;
+            }
+            l.remove(Character.valueOf(t.charAt(i)));
+        }
+        return true;
+    }
+}
