ad3596259a15d84338adea86fda3a29372809875
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-06-24 19:47:44 +0530

    Time: 10 ms (17.79%), Space: 47.2 MB (6.08%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,33 +1,60 @@
+import java.util.*;
+
 class Solution {
     public String reverseWords(String s) {
 
-        StringBuilder ans = new StringBuilder();
-        int i = s.length() - 1;
+        // StringBuilder ans = new StringBuilder();
+        // int i = s.length() - 1;
 
-        while(i >= 0) {
+        // while(i >= 0) {
 
-            while(i >= 0 && s.charAt(i) == ' ')
-                i--;
+        //     while(i >= 0 && s.charAt(i) == ' ')
+        //         i--;
 
-            if(i < 0)
-                break;
+        //     if(i < 0)
+        //         break;
 
-            StringBuilder word = new StringBuilder();
+        //     StringBuilder word = new StringBuilder();
 
-            while(i >= 0 && s.charAt(i) != ' ') {
-                word.append(s.charAt(i));
-                i--;
+        //     while(i >= 0 && s.charAt(i) != ' ') {
+        //         word.append(s.charAt(i));
+        //         i--;
+        //     }
+
+        //     ans.append(word.reverse());
+
+        //     while(i >= 0 && s.charAt(i) == ' ')
+        //         i--;
+
+        //     if(i >= 0)
+        //         ans.append(' ');
+        // }
+
+        // return ans.toString();
+
+
+        // using single loop:
+        int n = s.length();
+        String ans = "";
+        String rev = new StringBuilder(s).reverse().toString();
+        StringBuilder word = new StringBuilder();
+        for(int i =0 ;i<n;){
+            word.setLength(0);
+            while(i<n && rev.charAt(i) == ' '){
+                i++;
             }
-
-            ans.append(word.reverse());
-
-            while(i >= 0 && s.charAt(i) == ' ')
-                i--;
-
-            if(i >= 0)
-                ans.append(' ');
+            while( i<n &&rev.charAt(i) != ' '){
+                word.append(rev.charAt(i));
+                i++;
+            }
+            word.reverse();
+            while(i<n && rev.charAt(i) == ' '){
+                i++;
+            }
+            
+            ans += ' ' + word.toString();
+            
         }
-
-        return ans.toString();
+        return ans.substring(1);
     }
 }
