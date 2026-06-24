fdea10c59c7d5cac9aa9beb0bd183cf6e2309c94
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-06-24 18:14:45 +0530

    Time: 4 ms (82.18%), Space: 44.2 MB (82.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        List<Character> l = new ArrayList<>();
+        // List<Character> l = new ArrayList<>();
+        // int n = s.length();
+        // int m = t.length();
+        // if(n != m ){
+        //     return false;
+        // }
+        // for(int i=0 ;i<n;i++){
+        //         l.add(s.charAt(i));
+            
+        // }
+        // for(int i =0 ; i<m ;i++){
+        //     if(!l.contains(t.charAt(i))){
+        //         return false;
+        //     }
+        //     l.remove(Character.valueOf(t.charAt(i)));
+        // }
+        // return true;
         int n = s.length();
-        int m = t.length();
-        if(n != m ){
+        int z = t.length();
+        if(n!=z){
             return false;
         }
-        for(int i=0 ;i<n;i++){
-                l.add(s.charAt(i));
-            
+
+        int [] freq = new int[26];
+        for(int i =0 ;i<n ;i++ ){
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
-        for(int i =0 ; i<m ;i++){
-            if(!l.contains(t.charAt(i))){
+        for(int x : freq){
+            if(x!=0){
                 return false;
             }
-            l.remove(Character.valueOf(t.charAt(i)));
         }
         return true;
     }
