9e404dc5a27930af1e191a683c17ab567c0db3dd
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-06-29 12:38:16 +0530

    Time: 0 ms (100%), Space: 52.9 MB (57.1%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,44 +1,63 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
 
-        int n = nums.length-1;
-        int low = 0;
-        int high = n ;
-        if (nums.length == 1)
-        return nums[0];
+        // int n = nums.length-1;
+        // int low = 0;
+        // int high = n ;
+        // if (nums.length == 1)
+        // return nums[0];
 
-        while (low <= high) {
+        // while (low <= high) {
 
-            int mid = low + (high - low) / 2;
+        //     int mid = low + (high - low) / 2;
 
-            if (mid == 0 && nums[0] != nums[1])
-                return nums[mid];
+        //     if (mid == 0 && nums[0] != nums[1])
+        //         return nums[mid];
 
-            if (mid == n && nums[n ] != nums[n - 1])
-                return nums[n];
+        //     if (mid == n && nums[n ] != nums[n - 1])
+        //         return nums[n];
                 
-            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){
-                return nums[mid];
+        //     if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){
+        //         return nums[mid];
+        //     }
+
+        //     if (mid % 2 == 0) { 
+
+        //         if (nums[mid - 1] == nums[mid]) { 
+        //             high = mid - 1;
+        //         } else { 
+        //             low = mid + 1;
+        //         }
+
+        //     } else { 
+
+        //         if (nums[mid - 1] == nums[mid]) { 
+        //             low = mid + 1;
+        //         } else { 
+        //             high = mid - 1;  
+        //         }
+        //     }
+        // }
+
+        // return -1;
+
+        // another with simple code:
+        int n = nums.length - 1;
+        int low =0 ;
+        int high = n ;
+        while(low<high){
+            int mid = low + (high -low)/2;
+
+            if(mid%2 == 1){
+                mid--;
             }
-
-            if (mid % 2 == 0) { 
-
-                if (nums[mid - 1] == nums[mid]) { 
-                    high = mid - 1;
-                } else { 
-                    low = mid + 1;
-                }
-
-            } else { 
-
-                if (nums[mid - 1] == nums[mid]) { 
-                    low = mid + 1;
-                } else { 
-                    high = mid - 1;  
-                }
+            if(nums[mid] == nums[mid+1]){
+                low = mid+2;  // till yet the index are as usual not any break 
+            }
+            else{
+                high = mid;
             }
         }
-
-        return -1;
+        return nums[low];
     }
 }
