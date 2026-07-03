36e2e7c97134e77c0d1e3411abec2e1ed81356ec
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-07-03 14:47:32 +0530

    Time: 2 ms (96.13%), Space: 45.9 MB (18.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,34 +1,24 @@
 class Solution {
-
     List<List<Integer>> ans = new ArrayList<>();
-
-    public void solve(int index, int[] candidates, int target, List<Integer> curr) {
-
-        if (target == 0) {
-            ans.add(new ArrayList<>(curr));
+    public  void solve(int index , int[] candidate , int target , List<Integer> curr) {
+        if(target == 0){
+            ans.add(new ArrayList(curr));
             return;
         }
-
-        if (index == candidates.length) {
-            return;
+        if(index == candidate.length){
+            return ;
         }
+        if(candidate[index] <= target){
+            curr.add(candidate[index]);
 
-        if (candidates[index] <= target) {
-            curr.add(candidates[index]);
+            solve(index , candidate , target - candidate[index] , curr);
 
-            solve(index, candidates, target - candidates[index], curr);
-
-       
             curr.remove(curr.size() - 1);
         }
-
-        solve(index + 1, candidates, target, curr);
+        solve(index+1 , candidate , target , curr);
     }
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        solve(0, candidates, target, new ArrayList<>());
-
+        solve(0 , candidates, target , (new ArrayList<>()));
         return ans;
     }
 }
