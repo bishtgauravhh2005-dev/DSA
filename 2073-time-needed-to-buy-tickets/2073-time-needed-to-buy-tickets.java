39cb1586d87c4ce03c4a385eebb3251ad61237e8
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-07-23 21:33:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (99.87%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,31 +1,33 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i =0 ;i < tickets.length ; i++){
+        //     q.offer(i);
+        // }
         // int timeNeeded = 0;
-        // for(int i=0;i<tickets.length ;i++){
-        //     if(i<=k){
-        //         timeNeeded += Math.min(tickets[i] , tickets[k]);
-        //     }
-        //     else{
-        //         timeNeeded += Math.min(tickets[k]-1 , tickets[i]);
-        //     }
+        // while(!q.isEmpty()){
+        //     timeNeeded++;
+        //     int f = q.peek();
+        //     q.poll();
+        //     tickets[f]--;
+
+        //     if(k == f && tickets[f] == 0) return timeNeeded;
+        //     if(tickets[f] != 0) q.offer(f);
+
         // }
         // return timeNeeded;
 
-        Queue<Integer> q = new LinkedList<>();
-        for(int i =0 ;i < tickets.length ; i++){
-            q.offer(i);
-        }
-        int timeNeeded = 0;
-        while(!q.isEmpty()){
-            timeNeeded++;
-            int f = q.peek();
-            q.poll();
-            tickets[f]--;
 
-            if(k == f && tickets[f] == 0) return timeNeeded;
-            if(tickets[f] != 0) q.offer(f);
+        int time = 0 ;
+        for(int i =0 ;i<tickets.length; i++){
+            if(k>=i) {
+                time += Math.min(tickets[k] , tickets[i]);
+            }
+            else{
+                time+= Math.min(tickets[k] - 1 , tickets[i]);
+            }
 
         }
-        return timeNeeded;
+        return time;
     }
 }
