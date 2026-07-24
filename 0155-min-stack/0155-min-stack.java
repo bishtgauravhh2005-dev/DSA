520c32fac376716c4d39b04f759771c4ef26c605
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-07-24 15:46:31 +0530

    Time: 29 ms (82.21%), Space: 102.6 MB (15.71%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,39 +1,89 @@
 class MinStack {
-    class pair {
-        int val1;
-        int val2;
+    // class pair {
+    //     int val1;
+    //     int val2;
 
-        pair(int val1, int val2){
-            this.val1= val1;
-            this.val2 = val2;
-        }
-    }
+    //     pair(int val1, int val2){
+    //         this.val1= val1;
+    //         this.val2 = val2;
+    //     }
+    // }
 
-    Stack<pair> s = new Stack<>();
+    // Stack<pair> s = new Stack<>();
+    // public MinStack() {
+        
+    // }
+    
+    // public void push(int value) {
+    //     if(s.isEmpty()){
+    //         s.push(new pair(value , value));
+    //     }
+    //     else{
+    //         int minVal = Math.min(value , s.peek().val2);
+    //         s.push(new pair(value , minVal));
+    //     }
+    // }
+    
+    // public void pop() {
+    //     s.pop();
+    // }
+    
+    // public int top() {
+    //     return s.peek().val1;
+    // }
+    
+    // public int getMin() {
+    //     return s.peek().val2;
+    // }
+    // class pair {
+    //     int val1;
+    //     int val2;
+
+    //     pair(int val1, int val2){
+    //         this.val1= val1;
+    //         this.val2 = val2;
+    //     }
+    // }
+
+    Stack<Long> s = new Stack<>();
+    long minVal;
     public MinStack() {
         
     }
     
     public void push(int value) {
-        if(s.isEmpty()){
-            s.push(new pair(value , value));
-        }
-        else{
-            int minVal = Math.min(value , s.peek().val2);
-            s.push(new pair(value , minVal));
-        }
+       if(s.isEmpty()){
+            s.push((long)value);
+            minVal = value;
+       }
+       else if(minVal > value){
+            s.push(2L * value - minVal);
+            minVal = value;
+       }
+       else{
+            s.push((long)value);
+       }
     }
     
     public void pop() {
+       if(s.peek() < minVal){
+            minVal = 2L*minVal - s.peek();
+       }
+
         s.pop();
     }
     
     public int top() {
-        return s.peek().val1;
+        if(s.peek() < minVal){
+            return (int)minVal;
+        }
+        else{
+            return s.peek().intValue();
+        }
     }
     
     public int getMin() {
-        return s.peek().val2;
+        return (int)minVal;
     }
 }
 
