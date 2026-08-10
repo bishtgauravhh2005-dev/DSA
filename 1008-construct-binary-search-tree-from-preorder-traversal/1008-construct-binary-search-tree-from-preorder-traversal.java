bb3893db3890ef4f91ce176d7d137e3082ca0d5a
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-08-10 16:50:14 +0530

    Time: 0 ms (100%), Space: 43.3 MB (88.79%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,29 +14,50 @@
  * }
  */
 class Solution {
-    public TreeNode bstFromPreorder(int[] preorder) {
-        TreeNode root = new TreeNode(preorder[0]);
-        for(int i =1 ;i< preorder.length ; i++){
-            insert(root , preorder[i]);
+    // public TreeNode bstFromPreorder(int[] preorder) {
+    //     TreeNode root = new TreeNode(preorder[0]);
+    //     for(int i =1 ;i< preorder.length ; i++){
+    //         insert(root , preorder[i]);
+    //     }
+    //     return root;
+    // }
+    // public void insert(TreeNode root , int val){
+    //     if(root.val > val){
+    //         if(root.left == null){
+    //             root.left = new TreeNode(val);
+    //         }
+    //         else{
+    //             insert(root.left , val);
+    //         }
+    //     }
+    //     else{
+    //         if(root.right ==null ){
+    //             root.right = new TreeNode(val);
+    //         }
+    //         else{
+    //             insert(root.right , val);
+    //         }
+    //     }
+    // }
+
+
+    // more optimize approach 
+    int index = 0;
+    public TreeNode construct(int [] preorder , int upperbound){
+        if(index == preorder.length || preorder[index] > upperbound){
+            return null;
         }
+        TreeNode root = new TreeNode(preorder[index]);
+        index++;
+
+        root.left = construct(preorder, root.val);
+
+        root.right = construct(preorder, upperbound);
+
         return root;
+
     }
-    public void insert(TreeNode root , int val){
-        if(root.val > val){
-            if(root.left == null){
-                root.left = new TreeNode(val);
-            }
-            else{
-                insert(root.left , val);
-            }
-        }
-        else{
-            if(root.right ==null ){
-                root.right = new TreeNode(val);
-            }
-            else{
-                insert(root.right , val);
-            }
-        }
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return construct(preorder , Integer.MAX_VALUE);
     }
 }
