553fd38c61bfcfece77b7d3bde3c1c2a67a1081f
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-08-12 03:29:09 +0530

    Time: 54 ms (23.48%), Space: 144 MB (46.44%) - LeetHub

--- 1932-merge-bsts-to-create-single-bst/1932-merge-bsts-to-create-single-bst.java
+++ 1932-merge-bsts-to-create-single-bst/1932-merge-bsts-to-create-single-bst.java
@@ -1,106 +1,99 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    private HashMap<Integer , TreeNode > map = new HashMap<>();
+    private HashSet<Integer> leafNodevalues = new HashSet<>();
+    private HashSet<Integer> used = new HashSet<>();
+    private void merge(TreeNode root){
+        if(root == null){
+            return ;
+        }
+        if(root.left != null && map.containsKey(root.left.val)){
+            TreeNode ChildTree = map.get(root.left.val);
 
-    HashMap<Integer, TreeNode> map = new HashMap<>();
-    HashSet<Integer> leafValues = new HashSet<>();
-    HashSet<Integer> used = new HashSet<>();
+            if(!used.contains(ChildTree.val)){
+                root.left = ChildTree;
+                used.add(ChildTree.val);
+            }
+        }
 
+        if(root.right != null && map.containsKey(root.right.val)){
+            TreeNode ChildTree = map.get(root.right.val);
+
+            if(!used.contains(ChildTree.val)){
+                root.right = ChildTree;
+                used.add(ChildTree.val);
+            }
+        }
+        merge(root.left);
+        merge(root.right);
+    }
+
+
+
+    public boolean isValidBST(TreeNode root , long lower , long upper ){
+        if(root == null){
+            return true;
+        }
+        if(root.val <= lower || root.val >= upper){
+            return false;
+        }
+
+        return isValidBST(root.left , lower , root.val ) && isValidBST(root.right , root.val , upper);
+    }
     public TreeNode canMerge(List<TreeNode> trees) {
+        
 
-        // Store every tree by its root value
-        for (TreeNode root : trees) {
-            map.put(root.val, root);
+        for(TreeNode root : trees){
+            map.put(root.val , root);
         }
 
-        // Store all leaf values
-        for (TreeNode root : trees) {
-
-            if (root.left != null) {
-                leafValues.add(root.left.val);
+        for(TreeNode root : trees){
+            if(root.left != null){
+                leafNodevalues.add(root.left.val);
             }
 
-            if (root.right != null) {
-                leafValues.add(root.right.val);
+            if(root.right!= null){
+                leafNodevalues.add(root.right.val);
             }
         }
-
-        // Find the final root
         TreeNode root = null;
-
-        for (TreeNode node : trees) {
-
-            if (!leafValues.contains(node.val)) {
+        for(TreeNode node : trees){
+            if(!leafNodevalues.contains(node.val)){
                 root = node;
                 break;
             }
         }
 
-        // No unique final root
-        if (root == null) {
+        if(root == null){
             return null;
         }
 
-        // Mark final root as used
         used.add(root.val);
 
-        // Merge all possible trees
         merge(root);
 
-        // We must have used every tree
-        if (used.size() != trees.size()) {
+        if(used.size() != trees.size()){
             return null;
         }
-
-        // Validate BST
-        if (!isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE)) {
+        if(!isValidBST(root , Long.MIN_VALUE , Long.MAX_VALUE)){
             return null;
         }
-
         return root;
     }
 
-    private void merge(TreeNode root) {
-
-        if (root == null) {
-            return;
-        }
-
-        // If left is a leaf and another tree has this root
-        if (root.left != null && map.containsKey(root.left.val)) {
-
-            TreeNode childTree = map.get(root.left.val);
-
-            if (!used.contains(childTree.val)) {
-                root.left = childTree;
-                used.add(childTree.val);
-            }
-        }
-
-        // If right is a leaf and another tree has this root
-        if (root.right != null && map.containsKey(root.right.val)) {
-
-            TreeNode childTree = map.get(root.right.val);
-
-            if (!used.contains(childTree.val)) {
-                root.right = childTree;
-                used.add(childTree.val);
-            }
-        }
-
-        merge(root.left);
-        merge(root.right);
-    }
-
-    private boolean isValidBST(TreeNode root, long min, long max) {
-
-        if (root == null) {
-            return true;
-        }
-
-        if (root.val <= min || root.val >= max) {
-            return false;
-        }
-
-        return isValidBST(root.left, min, root.val)
-            && isValidBST(root.right, root.val, max);
-    }
+    
 }
