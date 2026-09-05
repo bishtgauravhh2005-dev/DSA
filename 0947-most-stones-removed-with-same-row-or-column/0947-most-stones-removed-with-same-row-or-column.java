6d41ae9f48353d872cb3060328774ac24927c5cb
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-09-05 23:15:37 +0530

    Time: 30 ms (43.18%), Space: 47 MB (30.06%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,26 +1,84 @@
 class Solution {
-    public void dfs(int[][] stones , int curr , boolean[] vis){
-        vis[curr] = true;
+    // brute force approach :
+    // public void dfs(int[][] stones , int curr , boolean[] vis){
+    //     vis[curr] = true;
 
-        for(int i=0 ;i<stones.length;i++){
+    //     for(int i=0 ;i<stones.length;i++){
 
-            int r = stones[curr][0];
-            int c = stones[curr][1];
-            if((!vis[i]) && (stones[i][0] == r || stones[i][1] == c) ){
-                dfs(stones , i , vis);
-            }
+    //         int r = stones[curr][0];
+    //         int c = stones[curr][1];
+    //         if((!vis[i]) && (stones[i][0] == r || stones[i][1] == c) ){
+    //             dfs(stones , i , vis);
+    //         }
+    //     }
+    // }
+//     public int removeStones(int[][] stones) {
+//         int n = stones.length;
+//         int g = 0;
+//         boolean [] vis = new boolean[stones.length];
+//         for(int i=0 ;i<n ;i++){
+//             if(!vis[i]){
+//                 dfs(stones , i , vis);
+//                 g++;
+//             }
+//         }
+//      return n-g;   
+//     }
+    int parent[];
+    int rank[];
+
+    public int find(int x){
+        if(parent[x] == x){
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+    public void union(int i , int j){
+        int parA = find(i);
+        int parB = find(j);
+        if(parA == parB){
+            return;
+        }
+        if(rank[parA] < rank[parB]){
+            parent[parA] = parB;
+        }
+
+        else if(rank[parA] > rank[parB]){
+            parent[parB] = parA;
+        }
+        else{
+            parent[parB] = parA;
+            rank[parA]++;
         }
     }
     public int removeStones(int[][] stones) {
         int n = stones.length;
-        int g = 0;
-        boolean [] vis = new boolean[stones.length];
-        for(int i=0 ;i<n ;i++){
-            if(!vis[i]){
-                dfs(stones , i , vis);
+        parent = new int[n];
+        rank = new int[n];
+
+        for(int i =0 ;i < n ;i++){
+            parent[i] = i ;
+            rank[i] = 0 ;
+        }
+
+        for(int i =0 ;i< n ;i ++){
+            for(int j = i+1 ; j<n ;j++){
+                int r = stones[i][0];
+                int c = stones[i][1];
+
+                if(stones[j][0] == r || stones[j][1] == c){
+                    union(i,j);
+                }
+            }
+        }
+
+        int g = 0 ;
+        for(int i =0 ;i <n ; i++){
+            if(find(i) == i){
                 g++;
             }
         }
-     return n-g;   
+        return n-g;
     }
 }
