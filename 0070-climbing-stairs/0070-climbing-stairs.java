1ceadb65b5ed27b0de7edab778484264e7ab1b16
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-09-10 11:41:09 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,12 +4,10 @@ class Solution {
             return n ;
         }
         int dp[] = new int[n+1];
-        dp[0] = 0 ;
+        dp[0] = 1 ;
         dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
 
-        for(int i =4 ;i<=n ;i++){
+        for(int i =2 ;i<=n ;i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
 
