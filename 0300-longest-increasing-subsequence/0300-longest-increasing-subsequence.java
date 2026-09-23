62a957b3b5a6676b1242cf2b0bf7add6e3590dac
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-09-23 11:28:56 +0530

    Time: 37 ms (74.58%), Space: 46.2 MB (44.33%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,29 +1,46 @@
 class Solution {
-    public int solve(int[]nums , int prev , int i , int[][] dp){
-        if(i == nums.length){
-            return 0;
-        }
+    // public int solve(int[]nums , int prev , int i , int[][] dp){
+    //     if(i == nums.length){
+    //         return 0;
+    //     }
 
-        if(dp[i][prev+1] != -1){
-            return dp[i][prev+1];
-        }
+    //     if(dp[i][prev+1] != -1){
+    //         return dp[i][prev+1];
+    //     }
 
-        int take = 0 ;
-        if(prev == -1 || nums[prev] < nums[i]){
-            take = 1 + solve(nums , i , i+1 , dp);
-        }
+    //     int take = 0 ;
+    //     if(prev == -1 || nums[prev] < nums[i]){
+    //         take = 1 + solve(nums , i , i+1 , dp);
+    //     }
 
-        int skip = solve(nums , prev , i+1 , dp);
+    //     int skip = solve(nums , prev , i+1 , dp);
 
-        return dp[i][prev+1] = Math.max(take , skip);
-    }
+    //     return dp[i][prev+1] = Math.max(take , skip);
+    // }
+    // public int lengthOfLIS(int[] nums) {
+    //     int dp[][] = new int[nums.length+1][nums.length+1];
+
+    //     for(int i=0 ;i<nums.length+1 ; i++){
+    //         Arrays.fill(dp[i] , -1);
+    //     }
+
+    //     return solve(nums , -1 , 0 , dp);
+    // }
+
+// Tabulation ::
     public int lengthOfLIS(int[] nums) {
-        int dp[][] = new int[nums.length+1][nums.length+1];
+      int[] dp = new int[nums.length];
 
-        for(int i=0 ;i<nums.length+1 ; i++){
-            Arrays.fill(dp[i] , -1);
+      Arrays.fill(dp , 1);
+      int maxlis = 0 ;
+      for(int i =0;i < nums.length ; i++){
+        for(int j = 0 ;j<i ; j++){
+            if(nums[j] < nums[i]){
+                dp[i] = Math.max(dp[i] , dp[j]+1);
+            }
         }
-
-        return solve(nums , -1 , 0 , dp);
+        maxlis = Math.max(dp[i] , maxlis);
+      }
+      return maxlis;
     }
 }
