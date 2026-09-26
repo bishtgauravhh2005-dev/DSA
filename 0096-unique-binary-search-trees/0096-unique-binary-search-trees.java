09e8bf40164e44093d0423be7790c3ba639f330b
bishtgauravhh2005-dev <bishtgauravhh2005@gmail.com>
2026-09-26 20:03:45 +0530

    Time: 0 ms (100%), Space: 41.8 MB (83.59%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -14,26 +14,46 @@ class Solution {
     // return ans;
     // }
 
-    public int solve(int n , int dp[]) {
-        if(n<=1){
-            return 1;
-        }
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
+    // Memoization Approach
 
-        int ans =0 ;
-        for(int i = 1 ;i<=n ;i++){
-            ans += solve(i-1 , dp) * solve(n-i , dp);
-        }
+    // public int solve(int n , int dp[]) {
+    //     if(n<=1){
+    //         return 1;
+    //     }
 
-    return dp[n] = ans;
-    }
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+
+    //     int ans =0 ;
+    //     for(int i = 1 ;i<=n ;i++){
+    //         ans += solve(i-1 , dp) * solve(n-i , dp);
+    //     }
+
+    // return dp[n] = ans;
+    // }
+    // public int numTrees(int n){
+    //     int dp[] = new int[n+1];
+    //     Arrays.fill(dp , -1);
+
+    //     return solve(n , dp);
+    // }
+
+
+    // Tabulation Approach.
+
     public int numTrees(int n){
-        int dp[] = new int[n+1];
-        Arrays.fill(dp , -1);
+        int[] dp = new int[n+1];
 
-        return solve(n , dp);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2 ; i<=n ;i++){
+            for(int j=1 ;j<=i; j++){
+                dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
+    return dp[n];
     }
 }
